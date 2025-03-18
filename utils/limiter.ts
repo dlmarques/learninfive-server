7a9a38bc62ed869102d2c24df09988ae0deacd0f
dlmarques <daniel.marquesedigital@gmail.com>
2025-03-18 22:31:00 +0000

    Setup rate limiting per IP

--- utils/limiter.ts
+++ utils/limiter.ts
@@ -2,8 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 50,
   standardHeaders: "draft-8",
-  legacyHeaders: false,
   message: "Requests limit are reached.",
 });
