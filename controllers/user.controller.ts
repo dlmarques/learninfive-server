6496508e05fbb9736e3eecfeecc90dc1a5246ea7
dlmarques <daniel.marquesedigital@gmail.com>
2025-03-18 22:08:47 +0000

    Create /edit-profile and /get-user endpoints

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -51,3 +51,71 @@ export const createUserProfile = async (req: Request, res: Response) => {
     res.status(200).send({ success: false, content: "Something went wrong" });
   }
 };
+
+export const editProfile = async (req: Request, res: Response) => {
+  const token = extractTokenFromHeaders(req);
+  const user = req.body as User;
+
+  const userId = decode(token)?.sub;
+
+  const database = client.db("users");
+
+  const users = database.collection<User>("user");
+
+  const userResult = await users.findOne({ userId });
+
+  const userUpdateResult = await users.updateOne(
+    { userId },
+    {
+      $set: {
+        ...userResult,
+        csLevel: user.csLevel,
+        goals: user.goals,
+        preferences: user.goals,
+        topicsToAvoid: user.topicsToAvoid,
+      },
+    }
+  );
+
+  if (userUpdateResult.modifiedCount > 0) {
+    res.status(200).send({
+      success: true,
+      content: "User edited successfully",
+      edited: true,
+    });
+    return;
+  }
+  res.status(200).send({
+    success: true,
+    content: "Somethin went wrong",
+    edited: false,
+  });
+  return;
+};
+
+export const getUserData = async (req: Request, res: Response) => {
+  const token = extractTokenFromHeaders(req);
+
+  const userId = decode(token)?.sub;
+
+  const database = client.db("users");
+
+  const users = database.collection<User>("user");
+
+  const userResult = await users.findOne({ userId });
+
+  if (userResult?._id) {
+    res.status(200).send({
+      success: true,
+      content: userResult,
+      exists: true,
+    });
+    return;
+  }
+  res.status(200).send({
+    success: true,
+    content: "User not found",
+    exists: false,
+  });
+  return;
+};
