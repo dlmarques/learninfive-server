7bc35575eb1c7217c8cf8affb2fc527a6606313f
dlmarques <daniel.marquesedigital@gmail.com>
2025-03-18 00:50:38 +0000

    Create user profile endpoint

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -3,6 +3,8 @@ import { decode } from "jsonwebtoken";
 import { extractTokenFromHeaders } from "../utils/extractTokenFromHeaders";
 import { User } from "../types/User";
 import { Request, Response } from "express";
+import { insertNewUser } from "../utils/insertNewUser";
+import { checkIfUserExists } from "../utils/checkIfUserExists";
 
 export const checkUserProfile = async (req: Request, res: Response) => {
   const token = extractTokenFromHeaders(req);
@@ -11,9 +13,9 @@ export const checkUserProfile = async (req: Request, res: Response) => {
 
   const database = client.db("users");
 
-  const users = database.collection<User>("users");
+  const users = database.collection<User>("user");
 
-  const userResult = await users.findOne({ userId: userId });
+  const userResult = await users.findOne({ userId });
 
   if (userResult?._id) {
     res.status(200).send({
@@ -21,10 +23,31 @@ export const checkUserProfile = async (req: Request, res: Response) => {
       content: "User already registered.",
       exists: true,
     });
+    return;
   }
   res.status(200).send({
     success: true,
     content: "User not registered.",
     exists: false,
   });
+  return;
+};
+
+export const createUserProfile = async (req: Request, res: Response) => {
+  const user = req.body as User;
+
+  const userExists = await checkIfUserExists(user.userId);
+
+  if (userExists) {
+    res.status(200).send({ success: false, content: "User already exists." });
+    return;
+  }
+
+  const insertUserResult = await insertNewUser(user);
+
+  if (insertUserResult) {
+    res.status(200).send({ success: true, content: "User created." });
+  } else {
+    res.status(200).send({ success: false, content: "Something went wrong" });
+  }
 };
--- routes/user.ts
+++ routes/user.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { checkUserProfile } from "../controllers/user.controller";
+import {
+  checkUserProfile,
+  createUserProfile,
+} from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.get("/check-user-profile", checkUserProfile);
+router.post("/complete-profile", createUserProfile);
 
 export default router;
