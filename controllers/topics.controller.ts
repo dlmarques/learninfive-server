3a9039d1a0a831187bdcd828e510f2a200680e2f
dlmarques <daniel.marquesedigital@gmail.com>
2025-03-19 19:10:06 +0000

    Answer quiz logic

--- controllers/topics.controller.ts
+++ controllers/topics.controller.ts
@@ -18,6 +18,8 @@ import { v4 as uuidv4 } from "uuid";
 import { getUserDataById } from "../utils/getUserDataById";
 import { extractPastTopics } from "../utils/extractPastTopics";
 import { UserDataToModel } from "../types/Model";
+import { insertTopicToUserPastTopics } from "../utils/insertTopicToUserPastTopics";
+import { insertAnswerToUser } from "../utils/insertAnswerToUser";
 
 export const requestAndSaveNewPublicTopic = async () => {
   const pastTopics = await getPreviousPublicTopics();
@@ -27,7 +29,7 @@ export const requestAndSaveNewPublicTopic = async () => {
   if (modelResponse) {
     const parsedResponse = JSON.parse(modelResponse);
 
-    const topic = {
+    const topic: Topic = {
       ...parsedResponse,
       date: new Date(),
       public: true,
@@ -56,8 +58,12 @@ export const requestAndSaveNewUserTopic = async (
   if (modelResponse) {
     const parsedResponse = JSON.parse(modelResponse);
 
-    const topic = {
+    const topic: Topic = {
       ...parsedResponse,
+      quiz: {
+        id: uuidv4(),
+        ...parsedResponse.quiz,
+      },
       date: new Date(),
       userId,
       id: uuidv4(),
@@ -66,7 +72,12 @@ export const requestAndSaveNewUserTopic = async (
 
     const insertUserTopicResult = await insertNewUserTopic(topic);
 
-    if (insertUserTopicResult) {
+    const insertTopicToUserPastTopicsResult = await insertTopicToUserPastTopics(
+      topic,
+      userId
+    );
+
+    if (insertUserTopicResult && insertTopicToUserPastTopicsResult) {
       console.log("New user topic generated and inserted sucessfully");
       return topic;
     } else {
@@ -117,17 +128,27 @@ const getUserTopic = async (token: string, res: Response) => {
 
   const result = await topic.find({ userId }).toArray();
 
+  const userData = await getUserDataById(userId);
+
   const lastTopic = result.find((topic) => {
     if (isToday(topic.date)) return topic;
   });
 
-  // TODO: check if user has answered quiz
+  const answeredQuiz = userData?.answeredQuizzes?.find((quiz) => {
+    if (quiz.id === lastTopic?.quiz.id) return quiz;
+  });
 
   if (lastTopic) {
-    res.status(200).send({ success: true, content: lastTopic });
+    if (answeredQuiz) {
+      const topic: Topic = {
+        ...lastTopic,
+        quiz: { ...lastTopic.quiz, userAnswer: answeredQuiz.correctness },
+      };
+      res.status(200).send({ success: true, content: topic });
+    } else {
+      res.status(200).send({ success: true, content: lastTopic });
+    }
   } else {
-    const userData = await getUserDataById(userId);
-
     if (userData) {
       const pastTopics = extractPastTopics(userData);
 
@@ -143,6 +164,43 @@ const getUserTopic = async (token: string, res: Response) => {
       );
 
       res.status(200).send({ success: true, content: topic });
+    }
+  }
+};
+
+export const answerQuiz = async (req: Request, res: Response) => {
+  const token = extractTokenFromHeaders(req);
+
+  const userId = decode(token)?.sub as string;
+
+  const { answer, topicId } = req.body;
+  const database = client.db("topics");
+
+  const topic = database.collection<Topic>("topic");
+
+  const result = await topic.findOne({ id: topicId, userId });
+
+  if (result) {
+    const { quiz } = result;
+    const isCorrect = quiz.rightAnswer === answer;
+
+    const insertAnswerToUserResult = await insertAnswerToUser(
+      userId,
+      quiz.id,
+      isCorrect
+    );
+
+    if (insertAnswerToUserResult) {
+      res.status(200).send({
+        success: true,
+        content: "Quiz answered correctly",
+        correct: isCorrect,
+      });
+    } else {
+      res.status(200).send({
+        success: false,
+        content: "Something went wrong",
+      });
     }
   }
 };
